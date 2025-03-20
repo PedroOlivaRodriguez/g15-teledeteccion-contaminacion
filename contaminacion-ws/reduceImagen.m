bb4da432b85f59d3f63a8650b6cbf81532ec036a
AYESA\poliva <poliva@ayesa.com>
2025-03-20 16:47:33 +0100

    crear imagenes de las 3 provincias

--- contaminacion-ws/reduceImagen.m
+++ contaminacion-ws/reduceImagen.m
@@ -1,8 +1,8 @@
 function imagen_reducida = reduceImagen(imagen)
-  [F, C] = size(imagen);
+  [F, C, I] = size(imagen);
   imagen = double(imagen);
 
-  imagen_reducida = imagen(1:20:F, 1:20:C);
+  imagen_reducida = imagen(1:2:F, 1:2:C);
 
   imagen_reducida = uint8(imagen_reducida);
 end
